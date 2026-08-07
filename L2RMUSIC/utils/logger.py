1142c81c99dea3c70824a86d3863cbc6a5de6a1c
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2026-08-07 22:20:43 +0530

    ❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳

--- L2RMUSIC/utils/logger.py
+++ L2RMUSIC/utils/logger.py
@@ -7,6 +7,12 @@ from config import LOGGER_ID
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
+        # Safe way to get query to prevent IndexError
+        try:
+            query = message.text.split(None, 1)[1]
+        except (IndexError, AttributeError):
+            query = "Unknown"
+
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
@@ -18,8 +24,9 @@ async def play_logs(message, streamtype):
 <b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
 <b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>ǫᴜᴇʀʏ :</b> {query}
 <b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
