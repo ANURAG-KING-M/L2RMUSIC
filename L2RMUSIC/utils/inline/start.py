8c5ab4303d55084afc14061e6c30e386e008717c
★彡[ᴀɴᴜʀᴀɢ ᴋɪɴɢ]彡★ <ar4592975@gmail.com>
2026-08-07 06:19:33 +0530

    ❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳

--- L2RMUSIC/utils/inline/start.py
+++ L2RMUSIC/utils/inline/start.py
@@ -35,7 +35,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text="🔥 ᴏᴡɴᴇʀ 🔥", config.OWNER_ID),
+            InlineKeyboardButton(text="🔥 ᴏᴡɴᴇʀ 🔥", user_id=config.OWNER_ID),
         ],
         [
             InlineKeyboardButton(text="🇮🇳 ʟᴀɴɢᴜᴀɢᴇ 🏳️‍🌈", callback_data="bot_info_data"),
